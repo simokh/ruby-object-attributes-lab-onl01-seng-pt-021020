52645479462a1dfd73e99f3d105fbd24e0db24ec
Simo Khouili <m.simokhouili@gmail.com>
2020-02-25 05:18:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+
+def name=(dog_name)
+  @name= dog_name
+end
+
+def name
+  @name
+end
+
+def breed= (dog_breed)
+  @breed= Dog.new
+  @breed= "Beagle"
+end
+
+def breed
+  @breed
+end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+
+def name=(person_name)
+  @name= person_name
+end
+
+def name
+  @name
+end
+
+def job= (perosn_job)
+  @job= Person.new
+  @job= "Singer"
+end
+
+def job
+  @job
+end
+
+end

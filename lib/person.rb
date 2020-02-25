f26f3936e82d8eed266e482de7671806ae0208ac
Simo Khouili <m.simokhouili@gmail.com>
2020-02-25 05:15:59 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+
+def name=(person_name)
+  @name= person_name
+end
+
+def name
+  @name
+end
+
+def breed= (dog_breed)
+  @breed= Person.new
+  @breed= "Beagle"
+end
+
+def breed
+  @breed
+end
+
+end

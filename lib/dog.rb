051f5cddaa5593057759d9039d227ae31e00aca4
simokh <m.simokhouili@gmail.com>
2020-04-28 23:06:19 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,45 @@
-class Dog
+# class Dog
+#
+# def name=(dog_name)
+#   @name= dog_name
+# end
+#
+# def name
+#   @name
+# end
+#
+# def breed= (dog_breed)
+#   @breed= Dog.new
+#   @breed= "Beagle"
+# end
+#
+# def breed
+#   @breed
+# end
+#
+# end
 
-def name=(dog_name)
-  @name= dog_name
-end
 
-def name
-  @name
-end
 
-def breed= (dog_breed)
-  @breed= Dog.new
-  @breed= "Beagle"
-end
 
-def breed
-  @breed
-end
 
-end
+class Dog 
+    
+    # attr_accessor :name, :breed
+
+    def name= name 
+        @name = name 
+    end 
+
+    def name 
+        @name 
+    end 
+
+    def breed= breed 
+        @breed = breed 
+    end 
+
+    def breed 
+        @breed 
+    end 
+end 
--- lib/person.rb
+++ lib/person.rb
@@ -1,20 +1,40 @@
-class Person
+# class Person
+#
+# def name=(person_name)
+#   @name= person_name
+# end
+#
+# def name
+#   @name
+# end
+#
+# def job= (perosn_job)
+#   @job= Person.new
+#   @job= "Singer"
+# end
+#
+# def job
+#   @job
+# end
+#
+# end
 
-def name=(person_name)
-  @name= person_name
-end
+class  Person 
+    # attr_accessor :name, :job
 
-def name
-  @name
-end
+    def name= name 
+        @name = name 
+    end 
 
-def job= (perosn_job)
-  @job= Person.new
-  @job= "Singer"
-end
+    def name 
+        @name 
+    end 
 
-def job
-  @job
-end
+    def job= job 
+        @job = job 
+    end 
 
-end
+    def job 
+        @job 
+    end
+end 
